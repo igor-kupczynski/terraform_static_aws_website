d4c3343ffe766810cf6bc84f735ce02dbadf2516
Igor Kupczyński <igor@kupczynski.info>
2017-02-24 22:57:11 +0100

    Cloudfront zone id is an output

--- main.tf
+++ main.tf
@@ -36,6 +36,10 @@ output "cloudfront_url" {
   value = "${aws_cloudfront_distribution.cdn.domain_name}"
 }
 
+output "cloudfront_zone_id" {
+  value = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
+}
+
 
 # --- Resource configuraiton -----------------------------------------------
 
