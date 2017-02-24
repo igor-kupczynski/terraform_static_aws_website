f7ec708360061e97cd087bbd9ce1d103acfe2bac
Igor Kupczyński <igor@kupczynski.info>
2017-02-24 23:04:34 +0100

    CDN domain name is an output

--- main.tf
+++ main.tf
@@ -36,7 +36,11 @@ output "cloudfront_url" {
   value = "${aws_cloudfront_distribution.cdn.domain_name}"
 }
 
-output "cloudfront_zone_id" {
+output "cloudfront_domain_name" {
+  value = "${aws_cloudfront_distribution.cdn.domain_name}"
+}
+
+output "cloudfront_hosted_zone_id" {
   value = "${aws_cloudfront_distribution.cdn.hosted_zone_id}"
 }
 
