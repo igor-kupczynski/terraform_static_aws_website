458df42c7e30f10ec4b83b74f680251a6f84819c
Igor Kupczyński <igor@kupczynski.info>
2020-04-18 17:29:52 +0200

    Migrate to terraform 0.12

--- invalidate_cache.tf
+++ invalidate_cache.tf
@@ -1,7 +1,7 @@
 # Lambda defined here invalidates CloudFront cache in case the cached object
 # in S3 is changed
 resource "aws_iam_role" "invalidate_cloud_front_role" {
-  count = "${var.ssl_certificate_arn != "" ? 1 : 0}"
+  count = var.ssl_certificate_arn != "" ? 1 : 0
   name  = "invalidate_cloud_front"
 
   assume_role_policy = <<EOF
@@ -22,7 +22,7 @@ EOF
 }
 
 resource "aws_iam_policy" "invalidate_cloud_front_policy" {
-  count       = "${var.ssl_certificate_arn != "" ? 1 : 0}"
+  count       = var.ssl_certificate_arn != "" ? 1 : 0
   name        = "invalidate_cloud_front_policy"
   description = "Allow to create CloudFront invalidations"
 
@@ -54,47 +54,48 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "invalidate_cloud_front_attachment" {
-  count      = "${var.ssl_certificate_arn != "" ? 1 : 0}"
-  role       = "${aws_iam_role.invalidate_cloud_front_role.name}"
-  policy_arn = "${aws_iam_policy.invalidate_cloud_front_policy.arn}"
+  count      = var.ssl_certificate_arn != "" ? 1 : 0
+  role       = aws_iam_role.invalidate_cloud_front_role[count.index].name
+  policy_arn = aws_iam_policy.invalidate_cloud_front_policy[count.index].arn
 }
 
 data "archive_file" "invalidate_cloud_front_lambda_zip" {
-  count       = "${var.ssl_certificate_arn != "" ? 1 : 0}"
+  count       = var.ssl_certificate_arn != "" ? 1 : 0
   type        = "zip"
   source_file = "${path.module}/invalidate_cloud_front.py"
   output_path = "${path.module}/invalidate_cloud_front_lambda.zip"
 }
 
 resource "aws_lambda_function" "invalidate_cloud_front_lambda" {
-  count            = "${var.ssl_certificate_arn != "" ? 1 : 0}"
-  filename         = "${data.archive_file.invalidate_cloud_front_lambda_zip.output_path}"
+  count            = var.ssl_certificate_arn != "" ? 1 : 0
+  filename         = data.archive_file.invalidate_cloud_front_lambda_zip[count.index].output_path
   function_name    = "inalidate_cloud_front_on_s3_change"
-  role             = "${aws_iam_role.invalidate_cloud_front_role.arn}"
+  role             = aws_iam_role.invalidate_cloud_front_role[count.index].arn
   handler          = "invalidate_cloud_front.handle_s3_change"
-  source_code_hash = "${data.archive_file.invalidate_cloud_front_lambda_zip.output_base64sha256}"
+  source_code_hash = data.archive_file.invalidate_cloud_front_lambda_zip[count.index].output_base64sha256
   runtime          = "python3.7"
 
   environment {
     variables = {
-      CLOUDFRONT_DISTRIBUTION_ID = "${aws_cloudfront_distribution.cdn.0.id}"
+      CLOUDFRONT_DISTRIBUTION_ID = aws_cloudfront_distribution.cdn[0].id
     }
   }
 }
 
 resource "aws_lambda_permission" "allow_s3_event_notifications" {
+  count         = var.ssl_certificate_arn != "" ? 1 : 0
   action        = "lambda:InvokeFunction"
-  function_name = "${aws_lambda_function.invalidate_cloud_front_lambda.function_name}"
+  function_name = aws_lambda_function.invalidate_cloud_front_lambda[count.index].function_name
   principal     = "s3.amazonaws.com"
-  source_arn    = "${aws_s3_bucket.storage_bucket.arn}"
+  source_arn    = aws_s3_bucket.storage_bucket.arn
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
-  count  = "${(var.ssl_certificate_arn != "" ? 1 : 0) * (var.invalidate_cloud_front_on_s3_update) }"
-  bucket = "${aws_s3_bucket.storage_bucket.id}"
+  count  = (var.ssl_certificate_arn != "" ? 1 : 0) * (var.invalidate_cloud_front_on_s3_update ? 1 : 0)
+  bucket = aws_s3_bucket.storage_bucket.id
 
   lambda_function {
-    lambda_function_arn = "${aws_lambda_function.invalidate_cloud_front_lambda.arn}"
+    lambda_function_arn = aws_lambda_function.invalidate_cloud_front_lambda[count.index].arn
     events              = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*"]
   }
 }
--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ variable ssl_certificate_arn {
 
 variable redirect_subdomain {
   default     = ""
-  description = "Redirect ${redirect_subdomain}.${domain} --> ${domain}. If not set do not redirect."
+  description = "Redirect redirect_subdomain.domain --> domain. If not set do not redirect."
 }
 
 variable invalidate_cloud_front_on_s3_update {
@@ -39,52 +39,52 @@ variable default_cdn_ttl {
 
 # --- Outputs --------------------------------------------------------------
 output "s3_url" {
-  value = "${aws_s3_bucket.storage_bucket.website_endpoint}"
+  value = aws_s3_bucket.storage_bucket.website_endpoint
 }
 
 output "s3_subdomain_url" {
-  value = "${aws_s3_bucket.redirect_subdomain.0.bucket}"
+  value = aws_s3_bucket.redirect_subdomain[0].bucket
 }
 
 output "cloudfront_url" {
-  value = "${aws_cloudfront_distribution.cdn.0.domain_name}"
+  value = aws_cloudfront_distribution.cdn[0].domain_name
 }
 
 output "cloudfront_domain_name" {
-  value = "${aws_cloudfront_distribution.cdn.0.domain_name}"
+  value = aws_cloudfront_distribution.cdn[0].domain_name
 }
 
 output "cloudfront_hosted_zone_id" {
-  value = "${aws_cloudfront_distribution.cdn.0.hosted_zone_id}"
+  value = aws_cloudfront_distribution.cdn[0].hosted_zone_id
 }
 
 output "cloudfront_subdomain_redirect_url" {
-  value = "${aws_cloudfront_distribution.cdn_redirect_subdomain.0.domain_name}"
+  value = aws_cloudfront_distribution.cdn_redirect_subdomain[0].domain_name
 }
 
 output "cloudfront_subdomain_redirect_domain_name" {
-  value = "${aws_cloudfront_distribution.cdn_redirect_subdomain.0.domain_name}"
+  value = aws_cloudfront_distribution.cdn_redirect_subdomain[0].domain_name
 }
 
 output "cloudfront_subdomain_redirect_hosted_zone_id" {
-  value = "${aws_cloudfront_distribution.cdn_redirect_subdomain.0.hosted_zone_id}"
+  value = aws_cloudfront_distribution.cdn_redirect_subdomain[0].hosted_zone_id
 }
 
 # --- Resource configuraiton -----------------------------------------------
 
 # Bucket to store the static website
 resource "aws_s3_bucket" "storage_bucket" {
-  bucket = "${var.domain}"
+  bucket = var.domain
   acl    = "public-read"
 
   website {
-    index_document = "${var.index_document}"
-    error_document = "${var.error_404_document}"
+    index_document = var.index_document
+    error_document = var.error_404_document
   }
 }
 
 resource "aws_s3_bucket_policy" "storage_bucket" {
-  bucket = "${aws_s3_bucket.storage_bucket.id}"
+  bucket = aws_s3_bucket.storage_bucket.id
 
   policy = <<EOF
 {
@@ -104,22 +104,22 @@ EOF
 
 # Bucket to redirect subdomain --> non-subdomain
 resource "aws_s3_bucket" "redirect_subdomain" {
-  count = "${var.redirect_subdomain != "" ? 1 : 0}"
+  count = var.redirect_subdomain != "" ? 1 : 0
 
   bucket = "${var.redirect_subdomain}.${var.domain}"
   acl    = "public-read"
 
   website {
-    redirect_all_requests_to = "${var.domain}"
+    redirect_all_requests_to = var.domain
   }
 }
 
 # Cloudfront in front of the main site
 resource "aws_cloudfront_distribution" "cdn" {
-  count = "${var.ssl_certificate_arn != "" ? 1 : 0}"
+  count = var.ssl_certificate_arn != "" ? 1 : 0
 
   origin {
-    domain_name = "${aws_s3_bucket.storage_bucket.website_endpoint}"
+    domain_name = aws_s3_bucket.storage_bucket.website_endpoint
     origin_id   = "origin-${var.domain}"
 
     # Secret sauce required for the aws api to accept cdn pointing to s3 website endpoint
@@ -134,16 +134,16 @@ resource "aws_cloudfront_distribution" "cdn" {
 
   enabled = true
 
-  default_root_object = "${var.index_document}"
+  default_root_object = var.index_document
 
   custom_error_response {
     error_code            = "404"
-    error_caching_min_ttl = "${var.default_cdn_ttl}"
+    error_caching_min_ttl = var.default_cdn_ttl
     response_code         = "404"
     response_page_path    = "/${var.error_404_document}"
   }
 
-  aliases = ["${var.domain}"]
+  aliases = [var.domain]
 
   price_class = "PriceClass_100"
 
@@ -161,7 +161,7 @@ resource "aws_cloudfront_distribution" "cdn" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
-    default_ttl            = "${var.default_cdn_ttl}"
+    default_ttl            = var.default_cdn_ttl
   }
 
   restrictions {
@@ -171,7 +171,7 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = "${var.ssl_certificate_arn}"
+    acm_certificate_arn      = var.ssl_certificate_arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1"
   }
@@ -179,10 +179,10 @@ resource "aws_cloudfront_distribution" "cdn" {
 
 # Cloudfront in front of the subdomain redirect bucket
 resource "aws_cloudfront_distribution" "cdn_redirect_subdomain" {
-  count = "${(var.ssl_certificate_arn != "" ? 1 : 0) * (var.redirect_subdomain != "" ? 1 : 0)}"
+  count = (var.ssl_certificate_arn != "" ? 1 : 0) * (var.redirect_subdomain != "" ? 1 : 0)
 
   origin {
-    domain_name = "${aws_s3_bucket.redirect_subdomain.website_endpoint}"
+    domain_name = aws_s3_bucket.redirect_subdomain[count.index].website_endpoint
     origin_id   = "origin-${var.redirect_subdomain}.${var.domain}"
 
     custom_origin_config {
@@ -213,7 +213,7 @@ resource "aws_cloudfront_distribution" "cdn_redirect_subdomain" {
     }
 
     viewer_protocol_policy = "allow-all"
-    default_ttl            = "${var.default_cdn_ttl}"
+    default_ttl            = var.default_cdn_ttl
   }
 
   restrictions {
@@ -223,7 +223,7 @@ resource "aws_cloudfront_distribution" "cdn_redirect_subdomain" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = "${var.ssl_certificate_arn}"
+    acm_certificate_arn      = var.ssl_certificate_arn
     ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1"
   }
